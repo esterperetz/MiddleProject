9c854c1dc82ff5e34f3c4933d666c298ba3e59c6
Rattoy <lieletinger@gmail.com>
2025-12-16 14:50:15 +0200

    create a class for saving user details locally.

---
+++ CommonProject/src/Entities/User.java
@@ -0,0 +1,28 @@
+package Entities;
+
+public class User {
+	private String userName;
+	private int password;
+	
+	public User(String userName,int password) {
+		
+		this.userName = userName;
+		this.password = password;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public int getPassword() {
+		return password;
+	}
+
+	public void setPassword(int password) {
+		this.password = password;
+	}
+}
