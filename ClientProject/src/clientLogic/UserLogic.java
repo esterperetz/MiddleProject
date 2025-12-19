de933e092e091a6e4054db43dc9168a64532f317
esterperetz <Ester.peretz99@gmail.com>
2025-12-19 02:38:24 +0200

    add request functions to userLogic

--- ClientProject/src/clientLogic/UserLogic.java
+++ ClientProject/src/clientLogic/UserLogic.java
@@ -1,5 +1,38 @@
 package clientLogic;
 
+import clientGui.ClientUi;
+import Entities.ActionType;
+import Entities.Request;
+import Entities.ResourceType;
+import Entities.Subscriber;
+
 public class UserLogic {
 
+	private final ClientUi client;
+
+	public UserLogic(ClientUi client) {
+	        this.client = client;
+	    }
+
+	public void getAllSubscribers() {
+		Request req = new Request(ResourceType.SUBSCRIBER, ActionType.GET_ALL, null, null);
+		client.sendRequest(req);
+	}
+
+	public void getSubscriberById(int id) {
+		Request req = new Request(ResourceType.SUBSCRIBER, ActionType.GET_BY_ID, id, null);
+		client.sendRequest(req);
+	}
+
+	public void registerSubscriber(Subscriber subscriber) {
+		System.out.println("three");
+		Request req = new Request(ResourceType.SUBSCRIBER, ActionType.REGISTER_SUBSCRIBER, null, subscriber);
+		client.sendRequest(req);
+	}
+
+	public void updateSubscriber(Subscriber subscriber) {
+		Request req = new Request(ResourceType.SUBSCRIBER, ActionType.UPDATE, subscriber.getSubscriber_id(),
+				subscriber);
+		client.sendRequest(req);
+	}
 }
