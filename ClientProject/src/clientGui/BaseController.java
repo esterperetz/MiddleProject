0c47359c8679946e96a93c54768701e5861b6362
idorotner222 <ido.rotner2@gmail.com>
2025-12-24 22:22:25 +0200

    check

--- ClientProject/src/clientGui/BaseController.java
+++ ClientProject/src/clientGui/BaseController.java
@@ -3,7 +3,7 @@ package clientGui;
 import clientGui.navigation.MainNavigator;
 
 public interface BaseController {
-	
+	//asdasdas
 	void setClientUi(ClientUi clientUi);
 	void setMainNavigator(MainNavigator navigator);
 }
