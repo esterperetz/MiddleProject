1375f843983f7d43ac73bd313b3811efed34b228
Rattoy <lieletinger@gmail.com>
2025-12-25 01:53:33 +0200

    Now can log in to Employee menu by username and password only.

--- ClientProject/src/clientGui/managerTeam/RestaurantLoginController.java
+++ ClientProject/src/clientGui/managerTeam/RestaurantLoginController.java
@@ -5,6 +5,12 @@ import client.MessageListener;
 import clientGui.BaseController;
 import clientGui.ClientUi;
 import clientGui.navigation.MainNavigator;
+import clientGui.user.SubscriberOptionController;
+import clientLogic.EmployeeLogic;
+import entities.Alarm;
+import entities.Employee;
+import entities.Response;
+import javafx.application.Platform;
 
 // <--- השינוי החשוב
 
@@ -13,8 +19,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
 
 public class RestaurantLoginController extends MainNavigator implements MessageListener<Object> {
@@ -24,33 +32,56 @@ public class RestaurantLoginController extends MainNavigator implements MessageL
 
 	@FXML
 	private PasswordField passwordField;
-
+	
+	private ActionEvent currentEvent;
+	@FXML
+	private Employee employee;
+	
+	//if clicking in X add that server will knows and disccount this client.
+	@FXML
+	public void initialize() {
+	    Platform.runLater(() -> {
+	        if (usernameField.getScene() != null && usernameField.getScene().getWindow() != null) {
+	            Stage stage = (Stage) usernameField.getScene().getWindow();
+	            stage.setOnCloseRequest(event -> {
+	                clientUi.disconnectClient();
+	             
+	            });
+	        }
+	    });
+	}
+	
 	@FXML
 	void performLogin(ActionEvent event) {
-		
-		String username = usernameField.getText();
-		String password = passwordField.getText();
-		
-		if (this.clientUi == null) {
-			System.err.println("Error: No connection to server (clientUi is null). Cannot login.");
-			return;
+		try {
+			String username = usernameField.getText();
+			String password = passwordField.getText();
+			Platform.runLater(() -> {
+				employee = new Employee(username,password);
+	
+				System.out.println("Login attempt for: " + username);
+	
+				EmployeeLogic employeeLogic = new EmployeeLogic(clientUi);
+				this.currentEvent = event;
+				employeeLogic.getManagerByEmployee(employee);
+			  });
+			
+			
+		}catch(Exception e) {
+			System.out.println("Please enter valid input!");
 		}
-
-		// TODO: לוגיקת בדיקת סיסמה מול השרת (שלח בקשה לשרת)
-		// User user = new User(username, Integer.parseInt(password));
-		System.out.println("Login attempt for: " + username);
-
-		// --- מעבר מסך ---
+		
+	
 
 		// 1. שימוש בפונקציה הגנרית וקבלת הקונטרולר
-		ManagerOptionsController controller = super.loadScreen("managerTeam/workerOption", event,clientUi);
-
-		// 2. אתחול הנתונים במסך החדש
-		if (controller != null) {
-			controller.initData(clientUi,ManagerOptionsController.isManager());
-		} else {
-			System.err.println("Failed to load ManagerOptionsController. Check FXML path.");
-		}
+//		ManagerOptionsController controller = super.loadScreen("managerTeam/workerOption", event,clientUi);
+//
+//		// 2. אתחול הנתונים במסך החדש
+//		if (controller != null) {
+//			controller.initData(clientUi,ManagerOptionsController.isManager());
+//		} else {
+//			System.err.println("Failed to load ManagerOptionsController. Check FXML path.");
+//		}
 		/*
 		 * try { //moved to MainNavigation (the name of the
 		 * function:loadOrderTableScreen) FXMLLoader loader = new
@@ -77,11 +108,47 @@ public class RestaurantLoginController extends MainNavigator implements MessageL
 		// וודא שגם הקובץ SelectionScreen.fxml נמצא באותה תיקייה
 		super.loadScreen("navigation/SelectionScreen", event,clientUi);
 	}
+	
+	public void initData() {
+		
+	}
 
 	
 	@Override
 	public void onMessageReceive(Object msg) {
 		// TODO Auto-generated method stub
+		try{
+			if (msg instanceof Response) {
+				Response response = (Response)msg;
+			    Platform.runLater(() -> {
 
+			    	if(response.getStatus() == Response.ResponseStatus.SUCCESS) {
+			    		Alarm.showAlert("Login Succsesfully!", "Navigating to Manager Options...", AlertType.ERROR);
+						ManagerOptionsController controller = super.loadScreen("managerTeam/workerOption", currentEvent,clientUi);
+
+						// 2. אתחול הנתונים במסך החדש
+						if (controller != null) {
+							controller.initData(clientUi,ManagerOptionsController.isManager());
+						} else {
+							System.err.println("Failed to load ManagerOptionsController. Check FXML path.");
+						}
+					}
+					else {
+						Alarm.showAlert("Incorrect Input", "Your username or password is invalid!", AlertType.ERROR);
+					}
+    
+		            
+		        });
+			    if (msg instanceof String && "quit".equals(msg)) {
+					clientUi.disconnectClient();
+					return;
+				}
+				
+				
+			
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
---
+++ ClientProject/src/clientLogic/EmployeeLogic.java
@@ -0,0 +1,21 @@
+package clientLogic;
+
+import clientGui.ClientUi;
+import entities.ActionType;
+import entities.Employee;
+import entities.Request;
+import entities.ResourceType;
+
+public class EmployeeLogic {
+	private final ClientUi client;
+	
+	public EmployeeLogic(ClientUi client) {
+        this.client = client;
+    }
+	
+	public void getManagerByEmployee(Employee employee) {
+		
+		Request req = new Request(ResourceType.EMPLOYEE, ActionType.LOGIN, null, employee);
+		client.sendRequest(req);
+	}
+}
