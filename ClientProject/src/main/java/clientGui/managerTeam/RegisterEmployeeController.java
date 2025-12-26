0cabefa1bc29c1e0cb75c027c95ef4c38e96a333
esterperetz <Ester.peretz99@gmail.com>
2025-12-26 13:32:22 +0200

    add register employee form and controller

--- ClientProject/src/main/java/clientGui/managerTeam/RegisterEmployeeController.java
+++ ClientProject/src/main/java/clientGui/managerTeam/RegisterEmployeeController.java
@@ -1,19 +1,29 @@
 package clientGui.managerTeam;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import client.MessageListener;
+import clientGui.ClientUi;
 import clientGui.navigation.MainNavigator;
 import clientGui.user.SubscriberOptionController;
+import clientLogic.EmployeeLogic;
 import clientLogic.UserLogic;
 import entities.ActionType;
+import entities.Employee;
+import entities.Employee.Role;
 import entities.Response;
 import entities.Subscriber;
+import entities.Order.OrderStatus;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
-public class RegisterEmployeeController extends MainNavigator implements MessageListener<Object>{
+public class RegisterEmployeeController extends MainNavigator implements Initializable, MessageListener<Object> {
 	@FXML
 	private TextField txtUsername;
 
@@ -24,9 +34,24 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 	private TextField txtEmail;
 
 	@FXML
+	private ComboBox<Role> selectRole;
+
+	@FXML
 	private Label lblMessage;
 
 	private ActionEvent currentEvent; // Added to save the event for async navigation
+
+	private boolean isManager;
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		selectRole.getItems().setAll(Role.values());
+	}
+
+	public void initData(ClientUi clientUi, boolean isManager) {
+		this.clientUi = clientUi;
+		this.isManager = isManager;
+	}
 
 	/**
 	 * Handles the registration process when "Register Now" is clicked.
@@ -37,9 +62,10 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 		String username = txtUsername.getText();
 		String phone = txtPhone.getText();
 		String email = txtEmail.getText();
-		
-		this.currentEvent = event; // Save current event
+		Role role = selectRole.getValue();
 
+		this.currentEvent = event; // Save current event
+		selectRole.setValue(role);
 		// 2. Validate Input (Basic checks)
 		if (username.isEmpty() || phone.isEmpty() || email.isEmpty()) {
 			lblMessage.setText("Error: All fields are required!");
@@ -53,11 +79,12 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 			return;
 		}
 		try {
-//		clientUi.addListener(this);//MUST DO NOT FORGER
-		UserLogic user = new UserLogic(clientUi);//MUST DO NOT FORGER
-		user.registerSubscriber(new Subscriber(0, username, phone, email)); //CHANGED FROM 123456 TO 0 (AUTO INC)
-		} catch(Exception e) {
-			System.out.println("one ");
+			EmployeeLogic employee = new EmployeeLogic(clientUi);
+			employee.registerEmployee(new Employee(username, "newEmployee1234",phone, email, role)); // CHANGED FROM
+																										// 123456 TO 0
+																										// (AUTO INC)
+		} catch (Exception e) {
+			System.out.println("employee register ");
 		}
 
 	}
@@ -65,25 +92,25 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 	@Override
 	public void onMessageReceive(Object msg) {
 		try {
-		if (msg instanceof Response) {
-			Response res = (Response) msg;
-			System.out.println(res.getStatus().getString());
-			
-			// Handle successful registration and navigate to Subscriber Options
-			if (res.getAction() == ActionType.REGISTER_SUBSCRIBER && res.getStatus() == Response.ResponseStatus.SUCCESS) {
-				Platform.runLater(() -> {
-					Subscriber newSub = (Subscriber) res.getData();
-					SubscriberOptionController controller = super.loadScreen("user/SubscriberOption", currentEvent, clientUi);
-					if (controller != null) {
-						controller.initData(clientUi, true, newSub.getSubscriberId());
-					}
-				});
-			} else if (res.getStatus() == Response.ResponseStatus.ERROR) {
-				Platform.runLater(() -> lblMessage.setText(res.getMessage_from_server()));
-			}
-		} else
-			System.out.println("nothing works");
-		} catch(Exception e) {
+			if (msg instanceof Response) {
+				Response res = (Response) msg;
+				// Handle successful registration and navigate to Employee Options
+				if (res.getAction() == ActionType.REGISTER_EMPLOYEE
+						&& res.getStatus() == Response.ResponseStatus.SUCCESS) {
+					Platform.runLater(() -> {
+						// will send a mail to the employee to make his own password
+						ManagerOptionsController controller = super.loadScreen("managerTeam/EmployeeOption",
+								currentEvent, clientUi);
+						if (controller != null) {
+							controller.initData(clientUi, isManager);
+						}
+					});
+				} else if (res.getStatus() == Response.ResponseStatus.ERROR) {
+					Platform.runLater(() -> lblMessage.setText(res.getMessage_from_server()));
+				}
+			} else
+				System.out.println("nothing works");
+		} catch (Exception e) {
 			System.out.println("two ");
 		}
 
@@ -95,9 +122,13 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 	@FXML
 	void handleBackBtn(ActionEvent event) {
 		// Fixed: Navigate back to SelectionScreen instead of Manager Dashboard
-		super.loadScreen("managerTeam/EmployeeOption", event, clientUi);
+		ManagerOptionsController controller = super.loadScreen("managerTeam/EmployeeOption", event, clientUi);
+		try {
+			controller.initData(clientUi, this.isManager);
+		} catch (NullPointerException e) {
+			System.err.println("Error: Could not load ManagerOptionsController.");
+		}
 	}
-	
 
 	/**
 	 * Clears the input fields after successful registration.
@@ -107,6 +138,5 @@ public class RegisterEmployeeController extends MainNavigator implements Message
 		txtPhone.clear();
 		txtEmail.clear();
 	}
-
 
 }
