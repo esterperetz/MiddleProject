95180dcb3dce7cfad6105a3aea590cc05ce437fb
idorotner222 <ido.rotner2@gmail.com>
2026-01-02 21:51:05 +0200

    fix make reservation

--- ClientProject/src/main/java/clientLogic/OrderLogic.java
+++ ClientProject/src/main/java/clientLogic/OrderLogic.java
@@ -8,53 +8,53 @@ import entities.ResourceType;
 
 public class OrderLogic {
 
-    private final ClientUi client;
+	private final ClientUi client;
 
-    public OrderLogic(ClientUi client) {
-        this.client = client;
-    }
+	public OrderLogic(ClientUi client) {
+		this.client = client;
+	}
 
+	public void getAllOrders() {
+		Request req = new Request(ResourceType.ORDER, ActionType.GET_ALL, null, null);
+		client.sendRequest(req);
+	}
 
-    public void getAllOrders() {
-        Request req = new Request(ResourceType.ORDER, ActionType.GET_ALL, null, null);
-        client.sendRequest(req);
-    }
+	public void getOrderById(int orderId) {
+		Request req = new Request(ResourceType.ORDER, ActionType.GET_BY_ID, orderId, null);
+		client.sendRequest(req);
+	}
 
-
-    public void getOrderById(int orderId) {
-        Request req = new Request(ResourceType.ORDER, ActionType.GET_BY_ID, orderId, null);
-        client.sendRequest(req);
-    }
-    
 	public void getOrdersBySubscriberId(int subscriberId) {
 		Request req = new Request(ResourceType.ORDER, ActionType.GET_ALL_BY_SUBSCRIBER_ID, subscriberId, null);
 		client.sendRequest(req);
 	}
 
-
-    public void createOrder(Order order) {
-        Request req = new Request(ResourceType.ORDER, ActionType.CREATE, null, order);
+   public void createOrder(Object data) {
+        Request req = new Request(ResourceType.ORDER, ActionType.CREATE, null, data);
         client.sendRequest(req);
     }
+//	public void createRes(Object data) {
+//		Request req = new Request(ResourceType.ORDER, ActionType.CREATE, null, data);
+//		client.sendRequest(req);
+//	}
 
+	public void updateOrder(Order order) {
+		Request req = new Request(ResourceType.ORDER, ActionType.UPDATE, order.getOrderNumber(), order);
+		client.sendRequest(req);
+	}
 
-    public void updateOrder(Order order) {
-        Request req = new Request(ResourceType.ORDER, ActionType.UPDATE, order.getOrderNumber(), order);
-        client.sendRequest(req);
-    }
+	public void deleteOrder(int orderId) {
+		Request req = new Request(ResourceType.ORDER, ActionType.DELETE, orderId, null);
+		client.sendRequest(req);
+	}
 
+	public void getSubscriberHistory(int subscriberId) {
+		Request req = new Request(ResourceType.ORDER, ActionType.GET_USER_ORDERS, null, subscriberId);
+		client.sendRequest(req);
+	}
 
-    public void deleteOrder(int orderId) {
-        Request req = new Request(ResourceType.ORDER, ActionType.DELETE, orderId, null);
-        client.sendRequest(req);
-    }
-    public void getSubscriberHistory(int subscriberId) {
-        Request req = new Request(ResourceType.ORDER, ActionType.GET_USER_ORDERS, null, subscriberId);
-        client.sendRequest(req);
-    }
-    
-    public void sendEmail(Order order) {
-    	 Request req = new Request(ResourceType.ORDER, ActionType.SEND_EMAIL, null, order);
-         client.sendRequest(req);
-    }
+	public void sendEmail(Order order) {
+		Request req = new Request(ResourceType.ORDER, ActionType.SEND_EMAIL, null, order);
+		client.sendRequest(req);
+	}
 }
