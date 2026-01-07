95c6e727cedabf89e63e1b1b7c51f5c2991f8434
IdoRonter222 <Ido.Rotner2@gmail.com>
2026-01-07 22:03:06 +0200

    add function that return the status the been clicked in the alarm

--- CommonProject/src/main/java/entities/Alarm.java
+++ CommonProject/src/main/java/entities/Alarm.java
@@ -1,6 +1,8 @@
 package entities;
 
 import javafx.scene.control.Alert;
+import java.util.Optional;
+import javafx.scene.control.ButtonType;
 
 public class Alarm {
 	public static void showAlertWithException(String header_text,String context_text,Alert.AlertType type,Exception e)
@@ -17,5 +19,12 @@ public class Alarm {
         alert.setContentText(context_text);
         alert.showAndWait();
 	}
+	public static Optional<ButtonType> showAlertAndConformation(String header_text, String context_text, Alert.AlertType type) {
+        Alert alert = new Alert(type);
+        alert.setHeaderText(header_text);
+        alert.setContentText(context_text);
+        
+        return alert.showAndWait();
+    }
 
 }
