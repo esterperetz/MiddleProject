9364ae16859bb75493caf6a397e73ed75f588371
Rattoy <lieletinger@gmail.com>
2026-01-11 18:53:32 +0200

    added constructor

--- CommonProject/src/main/java/entities/OpeningHours.java
+++ CommonProject/src/main/java/entities/OpeningHours.java
@@ -2,19 +2,31 @@ package entities;
 
 import java.io.Serializable;
 import java.sql.Time;
+import java.util.Calendar;
 import java.util.Date;
 
 public class OpeningHours implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private int id;
-	private Integer dayOfWeek; // 1-7 (Sun-Sat)
+	private Date dateOfWeek; // 1-7 (Sun-Sat)
 	private Date specialDate; // Specific date override
 	private Time openTime;
 	private Time closeTime;
 	private boolean isClosed;
+	private Integer dayOfWeek;
+	
+	public OpeningHours(Date dateOfWeek, Date specialDate, Time openTime, Time closeTime) {
 
-	public OpeningHours(int id, Integer dayOfWeek, Date specialDate, Time openTime, Time closeTime, boolean isClosed) {
+		this.dateOfWeek = dateOfWeek;
+		this.specialDate = specialDate;
+		this.openTime = openTime;
+		this.closeTime = closeTime;
+		this.dayOfWeek = getDayOfWeek(dateOfWeek);
+
+	}
+
+	public OpeningHours(int id,Integer dayOfWeek, Date specialDate, Time openTime, Time closeTime, boolean isClosed) {
 		this.id = id;
 		this.dayOfWeek = dayOfWeek;
 		this.specialDate = specialDate;
@@ -32,6 +44,10 @@ public class OpeningHours implements Serializable {
 		this.id = id;
 	}
 
+	public Date getDateOfWeek() {
+		return dateOfWeek;
+	}
+	
 	public Integer getDayOfWeek() {
 		return dayOfWeek;
 	}
@@ -42,6 +58,17 @@ public class OpeningHours implements Serializable {
 
 	public Time getOpenTime() {
 		return openTime;
+	}
+	
+	public int getDayOfWeek(Date dateOfWeek) {
+	    Calendar cal = Calendar.getInstance();
+	    cal.setTime(dateOfWeek);
+	    
+	    // ב-Calendar, הקבוע SUNDAY הוא 1 באופן אוטומטי
+	    int dayNumber = cal.get(Calendar.DAY_OF_WEEK);
+	    
+	    return dayNumber; 
+	    // תוצאה: ראשון=1, שני=2 ... שבת=7
 	}
 
 	public Time getCloseTime() {
