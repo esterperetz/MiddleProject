a14ca5af4b91740c100dcae21a98ccc73d52c907
Rattoy <lieletinger@gmail.com>
2026-01-11 18:54:48 +0200

    changed they way of Employee GUI looks , and added employee logic to update hours.

--- ClientProject/src/main/java/clientLogic/EmployeeLogic.java
+++ ClientProject/src/main/java/clientLogic/EmployeeLogic.java
@@ -4,6 +4,7 @@ import clientGui.ClientUi;
 import entities.ActionType;
 import entities.Customer;
 import entities.Employee;
+import entities.OpeningHours;
 import entities.Request;
 import entities.ResourceType;
 
@@ -26,6 +27,11 @@ public class EmployeeLogic {
 	
 	public void updatePassword(Employee employee) {
 		Request req = new Request(ResourceType.EMPLOYEE, ActionType.UPDATE, null, employee);
+		client.sendRequest(req);
+	}
+	
+	public void createOpeningHours(OpeningHours openingHours) {
+		Request req = new Request(ResourceType.BUSINESS_HOUR, ActionType.CREATE, null, openingHours);
 		client.sendRequest(req);
 	}
 	
